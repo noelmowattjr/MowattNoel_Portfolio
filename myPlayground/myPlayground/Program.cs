c29b29477f8c16a3e63ca148186a95e1fa263085
Noel Mowatt <nwmowatt@fullsail.edu>
2016-10-31 12:46:16 -0400

    Playground initial commit
    My playground for practicing as I follow lessons on SoloLearn.com

---
+++ myPlayground/myPlayground/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5; int y = 12;
+            if (x > 10 || y/x > 1)
+            {
+                Console.WriteLine("y minus x is: "+(y-x));
+            }
+            else
+            {
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
