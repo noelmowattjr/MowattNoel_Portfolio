594d82f464496bdd1a7fe2b9025166f6d1f465a3
Noel Mowatt <nwmowatt@fullsail.edu>
2016-10-31 20:22:04 -0400

    Do while iteration
    Iteration of do while loop

--- myPlayground/myPlayground/Program.cs
+++ myPlayground/myPlayground/Program.cs
@@ -36,6 +36,20 @@ namespace myPlayground
                 Console.WriteLine("The sum of {0} + {1} equals to {2}", a, b, sum);
 
             } while (false);
+
+            //--Calculate the area of a triangle
+            do
+            {
+                Console.Write("Enter triangle's width: ");
+                int width = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter triangle's height: ");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                int area = (width * height) / 2;
+                Console.WriteLine("You entered {0} for your width and {1} for your height, thus giving us an area of {2} for your trianlge!", width, height, area);
+
+            } while (false);
         }
     }
 }
