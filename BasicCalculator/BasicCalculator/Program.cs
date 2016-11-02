6cb962ba2c3daa93818781e010d80122136a24cb
Noel Mowatt <nwmowatt@fullsail.edu>
2016-11-02 18:12:57 -0400

    Basic Calculator PJ Setup
    Initial commit for setup of Basic calculator console app

---
+++ BasicCalculator/BasicCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Noel Mowatt
+                November 2, 2016
+                Instructor: Edward
+                Class: Project & Portfolio I*/
+
+            /*  This will be a baic functioning calculator that will be strictly console based */
+        }
+    }
+}
