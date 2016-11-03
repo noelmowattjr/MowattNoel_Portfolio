2eec320adaf0d4f2112e81c1a5b4b3bfbf6c003c
Noel Mowatt <nwmowatt@fullsail.edu>
2016-11-03 13:13:18 -0400

    Created Arithmetic Class
    Created ArithmeticOperator class to house my arithmetic operators
    (+,-,/,*)

--- BasicCalculator/BasicCalculator/Program.cs
+++ BasicCalculator/BasicCalculator/Program.cs
@@ -16,6 +16,18 @@ namespace BasicCalculator
                 Class: Project & Portfolio I*/
 
             /*  This will be a baic functioning calculator that will be strictly console based */
+
+            //--Tell user what they can do in our app and catch response
+            Console.WriteLine("Welcome to Basic Calculator! In this app you are allowed to add, substract, divide, and multiply any two numbers to get a result.\r\nAlso, we you will be able to utilize the arithmetic operators on your keyboard to calculate. Let's get starter.");
+            string userInput1 = Console.ReadLine();
+
+            //--Indirect prompt and catch 
+            Console.WriteLine("");
+            string arithOperator = Console.ReadLine();
+
+        
+            
+        
         }
     }
 }
