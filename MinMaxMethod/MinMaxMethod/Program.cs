a14b73a31aee91124fd18a876238ab4657893386
Noel Mowatt <nwmowatt@fullsail.edu>
2016-11-08 21:57:06 -0500

    MinMax function...in progress
    Attempting to construct a method that takes in an array of numbers and
    spits out the minimum and maximum values!

---
+++ MinMaxMethod/MinMaxMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMaxMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //--findMinAndMax();
+        }
+
+        /*  A method for finding the min and max of an array of numbers */
+
+        static Tuple<int, int> findMinAndMax(int [] numbers)
+        {
+            //--my holders for my min and max number values
+            int minNow = numbers[0];
+            int maxNow = numbers[0];
+
+            //--use n to iterate through array called numbers
+            foreach (int n in numbers) {
+
+            }
+
+            return Tuple.Create(1,2);
+        }
+    }
+}
