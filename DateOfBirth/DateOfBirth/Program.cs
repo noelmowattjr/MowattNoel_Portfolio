592b1f72ca0dd806b1fc0b200330f9000fbb5baa
Noel Mowatt <nwmowatt@fullsail.edu>
2016-11-10 21:35:59 -0500

    Project setup
    Setting up project!

---
+++ DateOfBirth/DateOfBirth/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateOfBirth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Noel Mowatt
+                Course: DVP1
+                App: Date of Birth*/
+
+            /*  INSTRUCTIONS: user inputs DOB and the program outputs how old user is
+                in terms of hours and minutes */
+
+            
+        }
+    }
+}
