5e198d1fadf5e5cd6b136b6c03a6da432da57ae0
Noel Mowatt <nwmowatt@fullsail.edu>
2016-11-12 19:52:45 -0500

    User's Choice + Validation
    User now has a choice of whether to run the program or not. Also, I
    added a while loop for validating user’s Yes or No - User may only
    enter Yes or No to continue!

--- DateOfBirth/DateOfBirth/Program.cs
+++ DateOfBirth/DateOfBirth/Program.cs
@@ -16,17 +16,41 @@ namespace DateOfBirth
 
             /*  INSTRUCTIONS: user inputs DOB and the program outputs how old user is
                 in the form of hours and minutes */
-
-            
-
+                
             //--Prompt user for name
             Console.Write("Hello and what is your name? ");
             string name = Console.ReadLine();
-
             
-            //--Greet user by name
+            //--Greet user by name and according to time of day
             string greetByName = Greetings(name);
-            //--Console.WriteLine(greetByName);
+
+            //--Debrief user
+            Console.Write("Today we are going to find out how old you are in hours and minutes!\r\nType Yes to find out or No to exit! Ready? ");
+            string toBeOrNotToBe = Console.ReadLine();
+
+            //--VALIDATION: Yes OR No--//
+            while (toBeOrNotToBe != "YES" && toBeOrNotToBe != "Yes" && toBeOrNotToBe != "yes" && toBeOrNotToBe != "NO" && toBeOrNotToBe != "No" && toBeOrNotToBe != "no")
+            {
+                Console.Write("Please enter Yes OR No to continue: ");
+                toBeOrNotToBe = Console.ReadLine();
+            }
+
+            //--if user enters 'Yes' begin the program
+            if (toBeOrNotToBe == "YES" || toBeOrNotToBe == "Yes" || toBeOrNotToBe == "yes")
+            {
+                //--------------Program Begins Here-----------------//
+                Console.WriteLine("Hi and welcome!");
+
+
+            } 
+            
+            //--if user enters 'No' then End the program
+            else if (toBeOrNotToBe == "NO" || toBeOrNotToBe == "No" || toBeOrNotToBe == "no")
+            {
+                Console.WriteLine("Maybe next time. Goodbye!");
+                return;
+            }
+            
             
 
         }
@@ -35,31 +59,25 @@ namespace DateOfBirth
         static string Greetings(string me)
         {
             //--Time Object created--//
-            //--Depending on time of day - user will be greeted by good morning, goodafternoon or goodnight//
             DateTime timeOfDay = new DateTime();
 
             //--Instantiated my time variable to ouput real time
             timeOfDay = DateTime.Now;
-            //--Console.WriteLine("Today is {0}", timeOfDay);
             
             //----Filtering the time of day and greeting user accordingly-----//
             if (timeOfDay.Hour >= 0 && timeOfDay.Hour < 12)
             {
                 Console.WriteLine("Hi! And good morning {0}. Welcome to our Date of Birth Program!", me);
-            }
-            else if (timeOfDay.Hour >= 12 && timeOfDay.Hour < 16)
+            }else if (timeOfDay.Hour >= 12 && timeOfDay.Hour < 16)
             {
                 Console.WriteLine("Well, good afternoon {0}. Welcome to our Date of Birth Program!", me);
-            }
-            else if (timeOfDay.Hour >= 16 && timeOfDay.Hour < 18)
+            }else if (timeOfDay.Hour >= 16 && timeOfDay.Hour < 18)
             {
                 Console.WriteLine("Hello {0} and good evening! Welcome to our Date of Birth Program!", me);
-            }
-            else if (timeOfDay.Hour >= 18)
+            }else if (timeOfDay.Hour >= 18)
             {
                 Console.WriteLine("Greetings {0}! And good night. Welcome to our Date of Birth Program!", me);
-            }
-            return me;
+            }return me;
         }
     }
 }
