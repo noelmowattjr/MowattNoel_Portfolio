902d16979492695527478e7d112c931003e27c29
Noel Mowatt <nwmowatt@fullsail.edu>
2016-11-12 19:32:31 -0500

    Troubleshoot - Success
    Perfect! I closed out the program and reopened it! My code runs as
    intended!

--- DateOfBirth/DateOfBirth/Program.cs
+++ DateOfBirth/DateOfBirth/Program.cs
@@ -20,44 +20,46 @@ namespace DateOfBirth
             
 
             //--Prompt user for name
-            Console.Write("Hello and what is your name?");
+            Console.Write("Hello and what is your name? ");
             string name = Console.ReadLine();
 
+            
+            //--Greet user by name
+            string greetByName = Greetings(name);
+            //--Console.WriteLine(greetByName);
+            
+
+        }
+        
+        //--Greet user function
+        static string Greetings(string me)
+        {
             //--Time Object created--//
             //--Depending on time of day - user will be greeted by good morning, goodafternoon or goodnight//
             DateTime timeOfDay = new DateTime();
 
             //--Instantiated my time variable to ouput real time
             timeOfDay = DateTime.Now;
-
+            //--Console.WriteLine("Today is {0}", timeOfDay);
+            
             //----Filtering the time of day and greeting user accordingly-----//
             if (timeOfDay.Hour >= 0 && timeOfDay.Hour < 12)
             {
-                Console.WriteLine("Hi! And good morning {0}. Welcome to our Date of Birth Program!", name);
+                Console.WriteLine("Hi! And good morning {0}. Welcome to our Date of Birth Program!", me);
             }
             else if (timeOfDay.Hour >= 12 && timeOfDay.Hour < 16)
             {
-                Console.WriteLine("Well, good afternoon {0}. Welcome to our Date of Birth Program!", name);
+                Console.WriteLine("Well, good afternoon {0}. Welcome to our Date of Birth Program!", me);
             }
             else if (timeOfDay.Hour >= 16 && timeOfDay.Hour < 18)
             {
-                Console.WriteLine("Hello {0} and good evening! Welcome to our Date of Birth Program!", name);
+                Console.WriteLine("Hello {0} and good evening! Welcome to our Date of Birth Program!", me);
             }
             else if (timeOfDay.Hour >= 18)
             {
-                Console.WriteLine("Greetings {0}! And good night. Welcome to our Date of Birth Program!", name);
+                Console.WriteLine("Greetings {0}! And good night. Welcome to our Date of Birth Program!", me);
             }
-            //--Greet user by name
-            //--string greetByName = Greetings(name);
-
-
-        }
-        /*
-        //--Greet user function
-        static string Greetings(string me)
-        {
-            
             return me;
-        }*/
+        }
     }
 }
