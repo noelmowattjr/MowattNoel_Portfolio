c9777ff2369e4013aeab943f84a715baddb2e3a2
Noel Mowatt <nwmowatt@fullsail.edu>
2016-11-13 21:49:42 -0500

    Find Min/Max Logic completed
    Completed the logic for finding the min and max of my array of numbers.
    The method returns a tuple of min and max

--- MinMaxMethod/MinMaxMethod/Program.cs
+++ MinMaxMethod/MinMaxMethod/Program.cs
@@ -10,11 +10,14 @@ namespace MinMaxMethod
     {
         static void Main(string[] args)
         {
-            //--findMinAndMax();
+            //--Method returns a tuple of the minimal and maximum value of array
+            findMinAndMax(new int[] {1,2,3,4,5 });
+
+            //--Print min and max to console
+            Console.WriteLine("Results: "+ findMinAndMax(new int[] { 55,20,100,99,29,88 }));
         }
 
         /*  A method for finding the min and max of an array of numbers */
-
         static Tuple<int, int> findMinAndMax(int [] numbers)
         {
             //--my holders for my min and max number values
@@ -24,9 +27,18 @@ namespace MinMaxMethod
             //--use n to iterate through array called numbers
             foreach (int n in numbers) {
 
+                //--Filter out the min and max
+                if (n < minNow)
+                {
+                    minNow = n;
+                }
+                else if (n > maxNow)
+                {
+                    maxNow = n;
+                }
             }
 
-            return Tuple.Create(1,2);
+            return Tuple.Create(minNow, maxNow);
         }
     }
 }
